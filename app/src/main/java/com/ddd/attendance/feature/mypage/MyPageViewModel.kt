88e963fce1747f8f6d013d74eb3c52abe82ff2ec
isanghoony <isanghoony@gmail.com>
2025-04-18 23:45:34 +0900

    CreditModalSheet 추가

--- app/src/main/java/com/ddd/attendance/feature/mypage/MyPageViewModel.kt
+++ app/src/main/java/com/ddd/attendance/feature/mypage/MyPageViewModel.kt
@@ -3,6 +3,8 @@ package com.ddd.attendance.feature.mypage
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import javax.inject.Inject
 
 @HiltViewModel
@@ -10,4 +12,10 @@ class MyPageViewModel @Inject constructor(
     private val savedStateHandle: SavedStateHandle,
 ) : ViewModel() {
 
+    private val _credit: MutableStateFlow<Boolean> = MutableStateFlow(false)
+    val credit: StateFlow<Boolean> = _credit
+
+    fun setCredit(value: Boolean) {
+        _credit.value = value
+    }
 }
